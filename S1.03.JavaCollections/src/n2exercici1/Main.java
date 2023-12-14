d4800d03e30b6563f22e292609bb5110a12d7362
urier <orarpione@gmail.com>
2023-12-14 11:33:34 +0100

    "S1.03. N2E1"

---
+++ S1.03.JavaCollections/src/n2exercici1/Main.java
@@ -0,0 +1,38 @@
+package n2exercici1;
+
+import java.util.HashSet;
+
+public class Main {
+    static HashSet<Restaurant> llistaRestaurants = new HashSet<>();
+    public static void main (String[] args){
+
+        llistaRestaurants.add(new Restaurant ("Abac", 10));
+        afegeixSiNoExisteix(new Restaurant("Abac", 5));
+        afegeixSiNoExisteix(new Restaurant("Abac", 5));
+        afegeixSiNoExisteix(new Restaurant("Enigma", 10));
+        afegeixSiNoExisteix(new Restaurant("Xerta", 9));
+        afegeixSiNoExisteix(new Restaurant("Abac", 10)); //NO S'HA AFEGIT
+        imprimeixLlistaFinal();
+    }
+    static void afegeixSiNoExisteix (Restaurant nouRestaurant){
+        boolean existeix = comprovaSiExisteix(nouRestaurant);
+        if (!existeix){
+            llistaRestaurants.add(nouRestaurant);
+        }
+    }
+    static boolean comprovaSiExisteix (Restaurant nouRestaurant){
+        boolean existeix = false;
+        for (Restaurant restaurant : llistaRestaurants){
+            if ((restaurant.comparaNoms(nouRestaurant)) && restaurant.comparaPuntuacions(nouRestaurant)){
+                existeix = true;
+            }
+        }
+        return existeix;
+    }
+    static void imprimeixLlistaFinal (){
+        for (Restaurant restaurant : llistaRestaurants) {
+            System.out.println(restaurant.getNom() + " " + restaurant.getPuntuacio());
+        }
+    }
+
+}
---
+++ S1.03.JavaCollections/src/n2exercici1/Restaurant.java
@@ -0,0 +1,26 @@
+package n2exercici1;
+
+public class Restaurant {
+
+    final private String nom;
+    final private int puntuacio;
+
+    public Restaurant (String nom, int puntuacio){
+        this.nom = nom;
+        this.puntuacio = puntuacio;
+    }
+
+    public boolean comparaNoms (Restaurant nouRestaurant){
+        return this.nom.equalsIgnoreCase(nouRestaurant.getNom());
+    }
+    public boolean comparaPuntuacions (Restaurant nouRestaurant){
+        return this.puntuacio==nouRestaurant.getPuntuacio();
+    }
+    public String getNom (){
+        return nom;
+    }
+    public int getPuntuacio (){
+        return puntuacio;
+    }
+
+}
