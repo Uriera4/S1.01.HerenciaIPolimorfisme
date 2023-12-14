c7cabd23ae1a947d62eaecc7d144f575ff6c0988
urier <orarpione@gmail.com>
2023-12-14 16:07:06 +0100

    "S1.03. N2E2"

--- S1.03.JavaCollections/src/n2exercici2/Main.java
+++ S1.03.JavaCollections/src/n2exercici2/Main.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 public class Main {
     static HashSet<Restaurant> llistaRestaurants = new HashSet<>();
+
     public static void main (String[] args){
 
         llistaRestaurants.add(new Restaurant ("Abac", 10));
+        ompleLlistaHash();
+
+        ArrayList<Restaurant> llistaRestaurantsOrdenada = new ArrayList<>(llistaRestaurants);
+
+        ordenaEimprimeixLlista(llistaRestaurantsOrdenada, new CompararNoms());
+        System.out.println();
+        ordenaEimprimeixLlista(llistaRestaurantsOrdenada, new CompararPuntuacio().reversed());
+
+    }
+    static void ompleLlistaHash (){
         afegeixSiNoExisteix(new Restaurant("Abac", 5));
         afegeixSiNoExisteix(new Restaurant("Enigma", 5));
         afegeixSiNoExisteix(new Restaurant("Xerta", 9));
         afegeixSiNoExisteix(new Restaurant("Abac", 10));
-
-        TreeSet<Restaurant> llistaRestaurantsOrdenada = new TreeSet<>(new CompararNoms());
-        llistaRestaurantsOrdenada.addAll(llistaRestaurants);
-        imprimeixLlistaFinalOrdenada(llistaRestaurantsOrdenada); //ORDENADA PER NOMS
-        System.out.println();
-        TreeSet<Restaurant> llistaRestaurantsOrdenada2 = new TreeSet<>(new CompararPuntuacio());
-        llistaRestaurantsOrdenada2.addAll(llistaRestaurants);
-        imprimeixLlistaFinalOrdenada((TreeSet) llistaRestaurantsOrdenada2.descendingSet()); //ORDENADA PER PUNTUACIO
-
     }
     static void afegeixSiNoExisteix (Restaurant nouRestaurant){
         boolean existeix = comprovaSiExisteix(nouRestaurant);
@@ -38,8 +40,13 @@ public class Main {
         }
         return existeix;
     }
-    static void imprimeixLlistaFinalOrdenada (TreeSet<Restaurant> llistaRestaurantsOrdenada){
-        for (Restaurant restaurant : llistaRestaurantsOrdenada) {
+
+    static void ordenaEimprimeixLlista (ArrayList<Restaurant> llista, Comparator<Restaurant> restaurant){
+        llista.sort(restaurant);
+        imprimeixLlistaFinalOrdenada(llista);
+    }
+    static void imprimeixLlistaFinalOrdenada (ArrayList<Restaurant> llista){
+        for (Restaurant restaurant : llista) {
             System.out.println("Nom: " + restaurant.getNom() + ", Puntuacio: " + restaurant.getPuntuacio() + ".");
         }
     }
